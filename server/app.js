43cd4dd062228e66259a4486de8f9be7327db190
woobin0075 <woobin0075@gmail.com>
2021-09-08 20:46:10 +0900

    Feat: yml 추가 port 수정

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require(cors);
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 4000;
+const port = 80;
 
 app.use(
     cors({
