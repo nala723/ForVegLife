725a66ed1ae1c1181aa6c33ca1e325e94cb8713c
woobin0075 <woobin0075@gmail.com>
2021-09-08 17:07:03 +0900

    Feat: sequelize 모듈 추가, 초기 세팅

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require(cors);
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 4000;
+
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+        methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE']
+      })
+)
+
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+    res.status(201).send('hello world');
+})
+
+app.listen(port,()=>{
+    console.log('server running');
+})
