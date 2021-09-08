01b2af8122b35e4a46a61bf858f4ddc06ea6ca3d
woobin0075 <woobin0075@gmail.com>
2021-09-08 21:04:12 +0900

    Fix : app.js express.json 추가

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 80;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(
     cors({
         origin: true,
