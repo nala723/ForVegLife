92ebdb34ec82930b6228bad6e1ea704f8bfd964e
woobin0075 <woobin0075@gmail.com>
2021-09-09 13:13:50 +0900

    Feat: issue #3 자체 sign API 구현

--- server/controllers/user/signin.js
+++ server/controllers/user/signin.js
@@ -1,5 +1,36 @@
-const { User } = require("../../models");
+const { user } = require("../../models");
+const bcrypt = require('bcrypt');
+const { generateAccessToken, generateRefreshToken } = require('../tokenFunctions')
 
 module.exports = async (req, res) => {
-  res.send('로그인하기')
+  try {
+    const userInfo = await user.findOne({attributes: ['id', 'nickname', 'email', 'password', 'profile'], where : {email: req.body.email}});
+    const userProfile = userInfo.dataValues.profile;
+
+    if(!userInfo){
+      return res.status(401).json({message : "Invalid user or wrong password"});
+    }
+
+    bcrypt.compare(req.body.password, userInfo.dataValues.password).then((isMatch) => {
+      if(!isMatch){
+        return res.status(401).json({message : "Invalid user or wrong password"});
+      }else{
+          delete userInfo.dataValues.password;
+          delete userInfo.dataValues.profile;
+          const access_token = generateAccessToken(userInfo.dataValues);
+          const refresh_token = generateAccessToken(userInfo.dataValues);
+
+          res.cookie('RefreshToken', refresh_token, {httpOnly: true, sameSite: 'none', secure: true});
+          res.json({
+            message : 'ok',
+            accessToken : access_token,
+            profileblob : userProfile,
+            nickname : userInfo.dataValues.nickname
+          })
+      }
+    })
+
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -1,4 +1,41 @@
+const model = require('../../models');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+
 module.exports = async (req, res) => {
-    res.send('회원가입하기')
-  };
+    let {nickname, email, password} = req.body;
+
+    let profile = fs.readFileSync(
+      __basedir + "/resources/assets/tmp/bros_blank.jpg"
+    )
+
+    let user = await model.user.findOne({where : {email : email}});
+    if(user){
+      res.status(409).json({message : '이미 존재하는 이메일입니다'});
+    }else{
+      try {
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(password, salt, (err, hash) => {
+            if(err){
+              throw err;
+            }else{
+              model.user.create({
+                 nickname : nickname,
+                 password : hash,
+                 email : email,
+                 profile : profile
+              })
+              .then((result) => {
+                res.status(200).json({message : 'ok'})
+              })
+            }
+          })
+        })
+
+      } catch (error) { 
+        res.status(500).send(error)
+      }
+    }
+};
   
