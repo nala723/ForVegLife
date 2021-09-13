5c95d85808b16cae410149a1aeb344b0f2b8772f
woobin0075 <woobin0075@gmail.com>
2021-09-13 12:29:18 +0900

    Feat: #47 구글 로그인 사람의 정보 처리, 필요없는 파일 삭제

--- server/controllers/google/withdrawal.js
+++ server/controllers/google/withdrawal.js
@@ -1,4 +1,27 @@
+const { user } = require('../../models')
+
 module.exports = async (req, res) => {
-    res.send('구글회원탈퇴하기')
-  };
+    const authorization = req.headers['authorization'];
+    const email = req.body.email;
+
+    try {
+      const accessToken = authorization.split(' ')[1];
+      //토큰 없으면 구글 로그인한 사람이 아니므로 유저 아니라는 메세지 보내기 
+      if(!accessToken){
+        res.status(401).json({message : "invalid user"});
+
+      }else{
+        await user.destroy({
+          where : {
+            email : email
+          }
+        })
+        .then((result) => {
+          res.status(200).json({message : "success"})
+        })
+      }
+    } catch (error) {
+      res.status(500).send(error);
+    }
+};
   
