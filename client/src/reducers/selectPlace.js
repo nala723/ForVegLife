caeb59be21b55ee2e625139089102a6b623e79d1
hyeon3051 <hyeon3051@gmail.com>
2021-09-16 22:11:17 +0900

    Feat: redux 세팅

--- client/src/reducers/selectPlace.js
+++ client/src/reducers/selectPlace.js
@@ -12,10 +12,8 @@ export default function selectPlace(state = initialState, action) {
   switch (action.type) {
     case SELECTPLACE:
       return Object.assign({}, state, {
-       selectplace : {
         ...state.selectplace,
         ...action.payload.data,
-      }
     })
     default: {
       return state;
