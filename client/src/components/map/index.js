f896d374d31b04d1a8c96f4e96b78b269954c6e1
nala723 <enda11@naver.com>
2021-09-16 20:29:45 +0900

    feat/ fix reducer

--- client/src/components/map/index.js
+++ client/src/components/map/index.js
@@ -7,52 +7,52 @@ const { kakao } = window;
 const API_KEY = "8ae459a51f5b018322fee10f7aa86f24";
 
 export default function MapIndex({ latlng }) {
-  // const selectPlace = useSelector(state=> state.selectPlace)
-  // const MapCenter = useSelector(state => state.MapCenter)
-  // let lng = latlng.x !== 0 ? latlng.x : 127.10676860117488;
-  // let lat = latlng.y !== 0 ? latlng.y : 37.365264512305174;
-  // const dispatch = useDispatch();
-  // // useEffect(() => {
-  //   let address = "";
-  //   const container = document.getElementById("map");
-  //   let options = {
-  //     center: new kakao.maps.LatLng(lat, lng),
-  //     level: 4,
-  //   };
-  //   const map = new kakao.maps.Map(container, options);
-  //   const geocoder = new kakao.maps.services.Geocoder();
-  //   map.setDraggable(true);
-  //   kakao.maps.event.addListener(map, "tilesloaded", function () {
-  //     if (map.getCenter() > 4) {
-  //       return;
-  //     }
+  const selectPlace = useSelector(state=> state.selectplace)
+  const MapCenter = useSelector(state => state.MapCenter)
+  let lng = latlng.x !== 0 ? latlng.x : 127.10676860117488;
+  let lat = latlng.y !== 0 ? latlng.y : 37.365264512305174;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let address = "";
+    const container = document.getElementById("map");
+    let options = {
+      center: new kakao.maps.LatLng(lat, lng),
+      level: 4,
+    };
+    const map = new kakao.maps.Map(container, options);
+    const geocoder = new kakao.maps.services.Geocoder();
+    map.setDraggable(true);
+    kakao.maps.event.addListener(map, "tilesloaded", function () {
+      if (map.getCenter() > 4) {
+        return;
+      }
 
-  //     searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-  //     let markerPosition = new kakao.maps.LatLng(selectPlace.y, selectPlace.x);
-  //     let marker = new kakao.maps.Marker({
-  //       position: markerPosition,
-  //     });
-  //     marker.setMap(map);
-  //   });
+      searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+      let markerPosition = new kakao.maps.LatLng(selectPlace.y, selectPlace.x);
+      let marker = new kakao.maps.Marker({
+        position: markerPosition,
+      });
+      marker.setMap(map);
+    });
 
-  //   function searchAddrFromCoords(coords, callback) {
-  //     // 좌표로 행정동 주소 정보를 요청합니다
+    function searchAddrFromCoords(coords, callback) {
+      // 좌표로 행정동 주소 정보를 요청합니다
 
-  //     geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
-  //   }
-  //   function displayCenterInfo(result, status) {
-  //     if (status === kakao.maps.services.Status.OK) {
-  //       for (let i = 0; i < result.length; i++) {
-  //         // 행정동의 region_type 값은 'H' 이므로
-  //         if (result[i].region_type === "B") {
-  //           dispatch(mapCenter({ x: result[i].x, y: result[i].y }));
-  //           address = result[i].address_name;
-  //           console.log(address);
-  //         }
-  //       }
-  //     }
-  //   }
-  // }, [selectPlace.x, selectPlace.y]);
+      geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+    }
+    function displayCenterInfo(result, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        for (let i = 0; i < result.length; i++) {
+          // 행정동의 region_type 값은 'H' 이므로
+          if (result[i].region_type === "B") {
+            dispatch(mapCenter({ x: result[i].x, y: result[i].y }));
+            address = result[i].address_name;
+            console.log(address);
+          }
+        }
+      }
+    }
+  }, [selectPlace.x, selectPlace.y]);
   return (
     <>
       <Map id="map"></Map>
