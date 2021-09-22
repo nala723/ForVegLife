c389e01a4c5abb03b8f4ad369926bfdb42a101b0
nala723 <enda11@naver.com>
2021-09-22 21:50:35 +0900

    Fix: 헤더 수정 중

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import styled from "styled-components";
-
+import theme from "../styles/theme";
 import Mypage from "../pages/MyPage";
 import MapPage from "../pages/MapPage";
 import { useEffect, useState } from "react";
@@ -15,12 +15,15 @@ import { isLogin } from "../actions/index";
 import Login from "./user/login";
 import SignUp from "./user/sign-up";
 import NotFound from "../pages/NotFoundPage";
+import {Buffer} from 'buffer';
 
 const Navbar = () => {
   const loginState = useSelector((state) => state.userReducer);
   const dispatch = useDispatch();
   const [loginModal, setLoginModal] = useState(false);
   const [registerModal, setRegisterModal] = useState(false);
+  const userState = useSelector((state)=> state)
+  const {accessToken,profileblob} = loginState;
   const userLogin = () => {
     setLoginModal(true);
     console.log(loginModal);
@@ -35,20 +38,28 @@ const Navbar = () => {
     setRegisterModal(false);
   };
   const logout = () => {
-    dispatch(isLogin({ isLogin: false, email: null, nickName: null }));
+    dispatch(isLogin({ isLogin: false, email: null, nickName: null,accessToken: '',profileblob:""}));
     setLoginModal(false);
     setRegisterModal(false);
     window.location.href = "/";
   };
+
+  let profileIMG 
+  if(profileblob === null || Object.keys(profileblob).length ===0){
+      profileIMG = "/image/bros_blank.jpg"
+  }
+   else{ 
+      profileIMG = 'data:image/png;base64, '+ Buffer(profileblob,'binary').toString('base64')
+     };
 
   return (
     <>
       <Router>
         {loginState.isLogin ? (
           <Header>
-            <Logo>Logo</Logo>
-
-            <StyledLogin to="/" onClick={() => logout()}>
+             <Logo><img src="/image/logo.svg"/></Logo>  
+              <ButtonBox primary>
+            <StyledLogin to="/" onClick={() => logout()} >
               Logout
             </StyledLogin>
             <Route exact path="/">
@@ -57,19 +68,20 @@ const Navbar = () => {
             <Route path="/mypage">
               <StyledMypage to="/">map</StyledMypage>
             </Route>
-
-            <Image
-              src={''
-                  // "data:image/png;base64," +
-                  //   Buffer(loginState.profileblob, "binary").toString("base64")
-              }
-            />
+              <ImageBox>
+                <Image
+                  src={profileIMG}
+                />
+              </ImageBox>
+             </ ButtonBox >
           </Header>
         ) : (
           <Header>
-            <Logo>Logo</Logo>
+            <Logo><img src="/image/logo.svg"/></Logo>
+            <ButtonBox >
             <StyledLogin onClick={userLogin}>Login</StyledLogin>
             <StyledRegister onClick={Register}>Register</StyledRegister>
+            </ ButtonBox >
           </Header>
         )}
 
@@ -122,59 +134,72 @@ const Navbar = () => {
 };
 
 const Header = styled.header`
-  justify-content: center;
   align-items: center;
   background-color: none;
-  color: black;
-
   width: 100%;
-  height: 5vh;
+  height: 3.125rem;
+  padding:  1.688rem;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
 `;
-const Image = styled.image`
-  width: 5rem;
-  height: 5vh;
+const ImageBox = styled.div`
+  margin-left:0.5rem;
+  width: 2.3rem;
+  height: 2.3rem;
+  border-radius: 100%;
+  display: flex;
+  overflow: hidden;
+`;
+const Image = styled.img`
+   width: 100%;
+   height: 100%;
+   object-fit: cover;
+  background-color: grey;
 `;
 const Logo = styled.div`
-  width: 60%;
-  margin-left: 1rem;
-`;
-const StyledLogin = styled.div`
   display: flex;
-  width: 12%;
-  font-size: 0.2rem;
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  width: ${props=> props.primary ? '20rem' :'16.563rem'};
+  height: inherit;
   justify-content: center;
   align-items: center;
-  height: 3.5vh;
+  gap:1rem;
+  margin-right: 1rem;
+`;
+const StyledLogin = styled(Link)`
+  display: flex;
+  width: 7.375rem;
+  font-size:${theme.fonts.size.base};
+  font-family: ${theme.fonts.button};
+  font-weight: 500;
+  justify-content: center;
+  align-items: center;
+  height: 2.063rem;
   border-radius: 0.5rem;
   border: 1px solid #bbbbbb;
   background-color: white;
-  color: #7cb700;
+  color: ${theme.colors.green};
+  cursor:pointer;
+  :hover {
+       background-color: ${theme.colors.green};
+       color:white;
+       transition: all 0.5s ease;
+     }
 `;
-const StyledRegister = styled.div`
-  display: flex;
-  width: 12%;
-  justify-content: center;
-  font-size: 0.2rem;
-  align-items: center;
-  height: 3.5vh;
-  border: 1px solid #bbbbbb;
-  background-color: #7cb700;
-  border-radius: 0.5rem;
+const StyledRegister = styled(StyledLogin)`
+  background-color: ${theme.colors.green};
   color: white;
+  cursor:pointer;
+  :hover {
+       background-color: white;
+       color: ${theme.colors.green};
+     }
 `;
-const StyledMypage = styled(Link)`
-  display: flex;
-  width: 12%;
-  justify-content: center;
-  font-size: 0.2rem;
-  align-items: center;
-  height: 3.5vh;
-  border: 1px solid #bbbbbb;
-  background-color: #7cb700;
-  border-radius: 0.5rem;
-  color: white;
+const StyledMypage = styled(StyledRegister)`
+
 `;
 
 export default Navbar;
