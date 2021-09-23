fdf875bb37b5a2e42735806ac6be45b34d50f515
woobin0075 <woobin0075@gmail.com>
2021-09-23 13:39:43 +0900

    Feat: 추천 API 골격 작업

---
+++ server/controllers/restaurant/recommendation.js
@@ -0,0 +1,10 @@
+const models = require("../../models");
+const { isAuthorized } = require("../tokenFunctions");
+
+module.exports = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -8,5 +8,6 @@ router.get("/:placeId", controllers.select);
 router.post("/:placeId/like", controllers.createFavorites);
 router.delete("/:placeId/dislike", controllers.cancelFavorites)
 router.post("/:placeId/review", controllers.createReview)
+router.get("/restaurant", controllers.recommendation);
 
 module.exports = router;
